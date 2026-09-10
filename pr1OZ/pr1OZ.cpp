56e14fa0715913d3cefa1106115ec7100d1c8f87
Ra1nsss1 <ilukrostik20@gmail.com>
2026-09-10 11:50:20 +0300

    feat: реалізував класи персонажів

--- pr1OZ/pr1OZ.cpp
+++ pr1OZ/pr1OZ.cpp
@@ -26,3 +26,67 @@ public:
         cout << "атакує мечем!\n";
     }
 };
+//клас персонажа буде батьківським для персів
+class Character {
+protected:
+    IWeaponBehavior* weapon;
+
+public:
+    Character() : weapon(nullptr) {}
+
+    virtual ~Character() {
+        delete weapon;
+    }
+
+    void SetWeapon(IWeaponBehavior* newWeapon) {
+        if (weapon) {
+            delete weapon;
+        }
+        weapon = newWeapon;
+    }
+
+    virtual void Fight() const {
+        if (weapon) {
+            weapon->UseWeapon();
+        }
+        else {
+            cout << "Немає зброї в руках!\n";
+        }
+    }
+};
+// створення похідних класів персів
+class Knight : public Character {
+public:
+    Knight() {
+        weapon = new SwordBehavior();
+    }
+
+    void Fight() const override {
+        cout << "Лицар: ";
+        Character::Fight();
+    }
+};
+
+class King : public Character {
+public:
+    King() {
+        weapon = new BowBehavior();
+    }
+
+    void Fight() const override {
+        cout << "Король: ";
+        Character::Fight();
+    }
+};
+
+class Troll : public Character {
+public:
+    Troll() {
+        weapon = new AxeBehavior();
+    }
+
+    void Fight() const override {
+        cout << "Троль: ";
+        Character::Fight();
+    }
+};
